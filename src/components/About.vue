d06773edb5eda1f4896ef823f1a3bf734e208df7
CarolinB <carolin.bruederle@gmail.com>
2019-01-19 18:37:23 +0100

    added sample content to About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class="container">
-        About me ... 
+    <div class="container text-center">
+
+        <img src="../assets/mypic.jpg" class="img-fluid" alt="Responsive image">
+        <p class="about">
+            A little information about me and this website ... 
+        </p>
     </div>
 </template>
 
@@ -11,6 +15,12 @@
 </script>
 
 <style scoped>  /* About Styles only */
-    
+    .img-fluid {
+        max-height: 300px;
+    }
+
+    .about {
+        padding: 40px 0px;
+    }
 
 </style>
